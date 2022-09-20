97ac698b12eed941b496fa12f54961d669a05afc
mskk3215 <m.kaaiweb@gmail.com>
2022-09-20 19:49:56 +0900

    object commit2

--- js/main.js
+++ js/main.js
@@ -1,31 +1,9 @@
-"use strict"; //エラーチェックをする際にはエラーが発見しやすくなる
+'use strict'; //エラーチェックをする際にはエラーが発見しやすくなる
 
 {
-  class Post{
-    constructor(text){
-      this.text = text;
-      this.likeCount = 0;
+  document.querySelector("li").addEventListener("click",e=>{
+    if(e.target.nodeName ==="LI"){
+      e.target.classList.toggle("done");
     }
-    show(){
-      console.log(`${this.text}-${this.likeCount}likes`);
-    } 
-    like(){
-      this.likeCount++;
-      this.show();
-    }
-    static showInfo(){
-      console.log("Post class version 1.0");
-    }
-  
-  }
-
-
-  const posts =[
-    new Post("Javascriptの勉強中.."),
-    new Post("プログラミング楽しい!"),
-  ];
-
-  Post.showInfo();
-
+  });
 }
-
